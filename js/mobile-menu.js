df1bf2657af2cb709849d154df033f9972609ae9
Serhii Chuhunov <patricknamaste@gmail.com>
2022-07-28 10:45:47 +0300

    дз-8

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,14 +1,14 @@
 (() => {
-    const refs = {
-      openMobileMenuBtn: document.querySelector("[data-mobile-menu-open]"),
-      closeMobileMenuBtn: document.querySelector("[data-mobile-menu-close]"),
-      mobileMenu: document.querySelector("[data-mobile-menu]"),
-    };
-  
-    refs.openMobileMenuBtn.addEventListener("click", toggleMobileMenu);
-    refs.closeMobileMenuBtn.addEventListener("click", toggleMobileMenu);
-  
-    function toggleMobileMenu() {
-      refs.mobileMenu.classList.toggle("is-hidden");
-    }
-  })();
+  const refs = {
+    openMenuBtn: document.querySelector("[data-menu-open]"),
+    closeMenuBtn: document.querySelector("[data-menu-close]"),
+    menu: document.querySelector("[data-menu]"),
+  };
+
+  refs.openMenuBtn.addEventListener("click", toggleMenu);
+  refs.closeMenuBtn.addEventListener("click", toggleMenu);
+
+  function toggleMenu() {
+    refs.menu.classList.toggle("is-open");
+  }
+})();
